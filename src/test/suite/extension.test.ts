27b2d80806f7d5ca021c44e12c0bdd591a8c2804
Nikola Stevanovic <nikola.stevanovic@quectel.com>
2021-05-12 14:08:32 +0200

    Refactor extension tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -5,41 +5,43 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
-test('QuecPython: Extension Init', function(done) {
-    this.timeout(60 * 1000);
-    const extension = vscode.extensions.getExtension('Quectel.qpy-ide');
-    if (!extension.isActive) {
-        extension.activate().then((api) => {
+suite("QuecPython: Extension Tests", () => {
+    test('Extension Init', function(done) {
+        this.timeout(60 * 1000);
+        const extension = vscode.extensions.getExtension('Quectel.qpy-ide');
+        if (!extension.isActive) {
+            extension.activate().then((api) => {
+                done();
+            }, () => {
+                done('Failed to activate extension');
+            });
+        } else {
             done();
-        }, () => {
-            done('Failed to activate extension');
+        }
+    });
+
+    test('Command Registry', async () => {
+        return vscode.commands.getCommands(true).then((commands) => {
+            const QUECPYTHON_COMMANDS = [
+                'qpy-ide.refreshModuleFS',
+                'qpy-ide.openConnection',
+                'qpy-ide.closeConnection',
+                'qpy-ide.downloadFile',
+                'qpy-ide.setLineEnd',
+                'qpy-ide.toggleHexTranslation',
+                'qpy-ide.clearTerminal',
+                'qpy-ide.runScript',
+                'qpy-ide.removeFile',
+                'qpy-ide.removeDir',
+                'qpy-ide.createDir',
+            ];
+
+            const foundQpyCommands = commands.filter((value) => {
+                return QUECPYTHON_COMMANDS.indexOf(value) >= 0 || value.startsWith('qpy-ide.');
+            });
+
+            const errorMsg = 'Some QuecPython commands are not registered properly or a new command is not added to the test';
+            assert.strictEqual(foundQpyCommands.length, QUECPYTHON_COMMANDS.length, errorMsg);
         });
-    } else {
-        done();
-    }
-});
-
-test('QuecPython: Command Registry', async () => {
-    return vscode.commands.getCommands(true).then((commands) => {
-        const QUECPYTHON_COMMANDS = [
-            'qpy-ide.refreshModuleFS',
-            'qpy-ide.openConnection',
-            'qpy-ide.closeConnection',
-            'qpy-ide.downloadFile',
-            'qpy-ide.setLineEnd',
-            'qpy-ide.toggleHexTranslation',
-            'qpy-ide.clearTerminal',
-            'qpy-ide.runScript',
-            'qpy-ide.removeFile',
-            'qpy-ide.removeDir',
-            'qpy-ide.createDir',
-        ];
-
-        const foundQpyCommands = commands.filter((value) => {
-            return QUECPYTHON_COMMANDS.indexOf(value) >= 0 || value.startsWith('qpy-ide.');
-        });
-
-        const errorMsg = 'Some QuecPython commands are not registered properly or a new command is not added to the test';
-        assert.strictEqual(foundQpyCommands.length, QUECPYTHON_COMMANDS.length, errorMsg);
     });
 });
