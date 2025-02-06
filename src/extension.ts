45b9408226381c72429596a2dde67a221eef7123
Hassan-Mallah <hassansmallah@gmail.com>
2025-02-06 08:32:35 +0100

    Update tree view, and few sections

--- src/extension.ts
+++ src/extension.ts
@@ -13,6 +13,8 @@ export let fwProvider: FirmwareViewProvider;
 export async function activate(context: vscode.ExtensionContext) {
 	log(`${new Date().toLocaleString()} - registerTreeDataProvider activated`);
 	vscode.window.registerTreeDataProvider('qpyModuleFS', moduleFsTreeProvider);
+	vscode.window.registerTreeDataProvider('nodeDependencies', new TreeDataProvider());
+	vscode.window.registerTreeDataProvider('nodeDependenciestwo', new TreeDataProvider());
 	fwProvider = new FirmwareViewProvider(context.extensionUri);
 	openLog();
 	initStatusButtons();
@@ -27,3 +29,41 @@ export function deactivate() {
 	closeLog;
 }
 
+
+class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
+	onDidChangeTreeData?: vscode.Event<TreeItem|null|undefined>|undefined;
+  
+	data: TreeItem[];
+  
+	constructor() {
+	  this.data = [new TreeItem('cars', [
+		new TreeItem(
+			'Ford', [new TreeItem('Fiesta'), new TreeItem('Focus'), new TreeItem('Mustang')]),
+		new TreeItem(
+			'BMW', [new TreeItem('320'), new TreeItem('X3'), new TreeItem('X5')])
+	  ])];
+	}
+  
+	getTreeItem(element: TreeItem): vscode.TreeItem|Thenable<vscode.TreeItem> {
+	  return element;
+	}
+  
+	getChildren(element?: TreeItem|undefined): vscode.ProviderResult<TreeItem[]> {
+	  if (element === undefined) {
+		return this.data;
+	  }
+	  return element.children;
+	}
+  }
+  
+  class TreeItem extends vscode.TreeItem {
+	children: TreeItem[]|undefined;
+  
+	constructor(label: string, children?: TreeItem[]) {
+	  super(
+		  label,
+		  children === undefined ? vscode.TreeItemCollapsibleState.None :
+								   vscode.TreeItemCollapsibleState.Expanded);
+	  this.children = children;
+	}
+  }
